f679f2dcf6fa079fe5fddc574c26cde3ebd0634b
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-05-28 13:21:45 +0530

    contact section of frontend linked

--- solution/app.py
+++ solution/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form["full-name"]
+        email = request.form["email"]
+        message = request.form["message"]
+        
     return render_template("index.html")
 
 if __name__ == "__main__":
