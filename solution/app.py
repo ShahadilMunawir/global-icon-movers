4baa50e0be1252ae2ae442a160e3f4cd2b1881b0
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-05-30 00:27:06 +0530

    added admin.html in render_template in /admin

--- solution/app.py
+++ solution/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -19,5 +20,16 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "Shahadil" and password == "root@shahadil121#" or username == "Bodhi" and password == "bodhi_bo121":
+            cursor.execute("SELECT * FROM Contact")
+            data = cursor.fetchall()
+            return render_template("admin.html", os=os, username=username, data=data)
+    return render_template("login.html")
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5550)
+    app.run(debug=True, port=5555)
