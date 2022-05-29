ff7cdf8b8148c3e1925846b321e8fd49687704f4
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-05-29 23:14:21 +0530

    database connectivity added

--- solution/app.py
+++ solution/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+db = sqlite3.connect("Database.db", check_same_thread=False)
+cursor = db.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS Contact('ID' INTEGER PRIMARY KEY AUTOINCREMENT, 'Full Name' TEXT NOT NULL, 'Email' TEXT NOT NULL, 'Message' TEXT NOT NULL);")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -8,7 +14,9 @@ def index():
         name = request.form["full-name"]
         email = request.form["email"]
         message = request.form["message"]
-        
+        cursor.execute("INSERT INTO Contact('Full Name', 'Email', 'Message') VALUES (?, ?, ?);", (name, email, message))
+        db.commit()
+
     return render_template("index.html")
 
 if __name__ == "__main__":
