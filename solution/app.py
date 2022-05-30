035a3a5a3e3043aaf8d3e567eb9f70e9327e3cd5
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-05-30 08:40:57 +0530

    login and logout routes created with session

--- solution/app.py
+++ solution/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from datetime import timedelta
 import sqlite3
-import os
 
 app = Flask(__name__)
+app.secret_key = "complikeysecret#"
+app.permanent_session_lifetime = timedelta(seconds=600)
 
 db = sqlite3.connect("Database.db", check_same_thread=False)
 cursor = db.cursor()
@@ -22,14 +24,33 @@ def index():
 
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
+    if "username" in session:
+        username = session["username"]
+        flash(f"You are logged in as {username}")
+        cursor.execute("SELECT * FROM Contact")
+        data = cursor.fetchall()
+        return render_template("admin.html", data=data)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         if username == "Shahadil" and password == "root@shahadil121#" or username == "Bodhi" and password == "bodhi_bo121":
-            cursor.execute("SELECT * FROM Contact")
-            data = cursor.fetchall()
-            return render_template("admin.html", os=os, username=username, data=data)
+            session["username"] = username
+            return redirect(url_for("admin"))
+    else:
+        if "username" in session:
+            return redirect(url_for("admin"))
+    
     return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5555)
+    app.run(debug=True, port=5501)
